90e149769e57a9ff6b0e6cfb82120a5eefb0edf9
hippoandy <hippo1993928@gmail.com>
2018-01-25 21:11:33 -0800

    Modified main.js.

--- js/self/main.js
+++ js/self/main.js
@@ -7,36 +7,91 @@ var sub_dataset_1 = "./dataset/types.csv";
 var sub_dataset_2 = "./dataset/egggroups.csv";
 
 var data = [];
-
 var type_st = [];
 var eg_st = [];
 
-// Constants
-var diameter = 700,
-    format = d3.format( ",d" );
-
-// Define svg object
-var svg = d3.select( "#vis1" ).append( "svg" )
-    .attr( "width", diameter )
-    .attr( "height", diameter )
-    .attr( "class", "bubble" );
-
-// Define 'div' for tooltips
-var div = d3.select( "#vis1" )
-    // declare the tooltip div 
-    .append( "div" )
-    .attr( "class", "tooltip" )
-    .style( "opacity", 0 );
 // --------------------------------------------------- Variable Declarations
 
+var pokemons = document.getElementById( "pokemons" );
+pokemons.innerHTML = "";
+
+var numofpoke = 0;
+var numofpage = 0;
+var maxpokemon = 24;
+var recentbreak = 0;
+var page = 1;
+var numpage = document.getElementById( "pagenum" );
 // Reading Data ------------------------------------------------------------
 // read the main dataset
+var flag = 0;
 d3.csv( dataset, function( d )
 {
     data.push( d );
+
+    var ndiv = document.createElement( "div" );
+    ndiv.className = "item";
+    if( flag == 0 )
+    {
+        ndiv.className += " bgcolor-grass";
+        flag = 1;
+    }
+    else
+    {
+        ndiv.className += " bgcolor-grass-dark";
+        flag = 0;
+    }
+    var img = document.createElement( "img" );
+    img.setAttribute( "src", ("./img/pokemon/" + String(d.Number) + ".png") );
+    img.setAttribute( "style", "padding: 2px;" );
+    img.setAttribute( "width", "80px" );
+    img.setAttribute( "height", "80px" );
+    img.setAttribute( "alt", String(d.Name) );
+    ndiv.innerHTML = d.Name;
+    img.dataset.number = d.Number;
+    ndiv.appendChild( img );
+    ndiv.dataset.number = d.Number;
+    ndiv.onclick = find_pokemon;
+    pokemons.appendChild( ndiv );
+
+    numofpoke += 1;
+    // console.log( "Hi" + String(numofpoke / maxpokemon) );
+    if( numofpoke % maxpokemon == 0 )
+    {
+        recentbreak = numofpoke;
+        numofpage += 1;
+        $("#pokemons > div").slice( (numofpoke - maxpokemon), numofpoke ).addClass( 'page' + String(numofpage) ).hide();
+    }
+
 }, function( error, header )
 {
     if( error ) throw error;
+    $('.page1').show();
+    numpage.innerHTML = "1 / " + numofpage;
+
+    if( numofpoke % maxpokemon != 0 )
+    {
+        numofpage += 1;
+        $("#pokemons > div").slice( recentbreak, numofpoke ).addClass( 'page' + String(numofpage) ).hide();
+    }
+
+    $('.next').on('click', function()
+    {
+        if( page < numofpage )
+        {
+            $("#pokemons > div:visible").hide();
+            $('.page' + ++page).show();
+        }
+        numpage.innerHTML = page + " / " + numofpage;
+    });
+    $('.prev').on('click', function() {
+        if( page > 1 )
+        {
+            $("#pokemons > div:visible").hide();
+            $('.page' + --page).show();
+        }
+        numpage.innerHTML = page + " / " + numofpage;
+    });
+
     type_color();
 });
 
@@ -68,6 +123,23 @@ function eg_color()
 // start drawing the chart
 function draw()
 {
+    // Constants
+    var diameter = 700,
+    format = d3.format( ",d" );
+
+    // Define svg object
+    var svg = d3.select( "#vis1" ).append( "svg" )
+    .attr( "width", diameter )
+    .attr( "height", diameter )
+    .attr( "class", "bubble" );
+
+    // Define 'div' for tooltips
+    var div = d3.select( "#vis1" )
+    // declare the tooltip div 
+    .append( "div" )
+    .attr( "class", "tooltip" )
+    .style( "opacity", 0 );
+
     // Define a bubble object with attributes
     var bubble = d3.pack()
         .size( [diameter, diameter] )
@@ -130,15 +202,23 @@ function draw()
     })
     .on( "click", function( d ) {
         var list = document.getElementById( "list" );
+        list.style.visibility = "visible";
         list.innerHTML = "";
         for( var i = 0 ; i < data.length ; i++ )
         {
             if( data[ i ].Type_1 == d.data.Type )
             {
-                var new_div = document.createElement( "div" );
-                new_div.className = "item";
-                new_div.innerHTML = data[ i ].Name;
-                list.appendChild( new_div );
+                var ndiv = document.createElement( "div" );
+                ndiv.className = "item";
+                ndiv.innerHTML = data[ i ].Name;
+                var img = document.createElement( "img" );
+                img.setAttribute( "src", ("./img/pokemon/" + String(data[ i ].Number) + ".png") );
+                img.setAttribute( "style", "padding: 2px;" );
+                img.setAttribute( "width", "80px" );
+                img.setAttribute( "height", "80px" );
+                img.setAttribute( "alt", String(data[ i ].Name) );
+                ndiv.appendChild( img );
+                list.appendChild( ndiv );
             }
         }
     });
@@ -146,7 +226,54 @@ function draw()
 }
 
 // show the first tab
-document.getElementById( "Home" ).style.display = "block";
+document.getElementById( "dashboard" ).style.display = "block";
+
+function find_pokemon( event )
+{
+    var el = event.target;
+    var num = parseInt(el.dataset.number);
+    var ava = document.getElementById( "avatar" );
+    ava.innerHTML = ""
+
+    var img = document.createElement( "img" );
+    img.setAttribute( "src", ("./img/pokemon/" + String(num) + ".png") );
+    img.setAttribute( "style", "padding: 10px;" );
+    img.setAttribute( "width", "180px" );
+    img.setAttribute( "height", "180px" );
+    ava.appendChild( img );
+
+    var cir = document.getElementById( "det-cir" );
+    cir.style.fill = data[ (num - 1) ].Color;
+
+    var name = document.getElementById( "det-name" );
+    name.innerHTML = data[ (num - 1) ].Name;
+
+    var det = document.getElementById( "det" );
+    var toput = "<table class='det-text' width='100%'><tr><td width='30%' align='center' sytle='margin-right: 20px;'>Type</td><td><type>"
+    if( data[ (num - 1) ].Type_2 == "" )
+        toput += data[ (num - 1) ].Type_1;
+    else
+        toput += data[ (num - 1) ].Type_1 + " / " + data[ (num - 1) ].Type_2;
+    toput += "</type></td></tr><tr><td align='center'>Egg Group</td><td><type>";
+    if( data[ (num - 1) ].Egg_Group_2 == "" )
+        toput += data[ (num - 1) ].Egg_Group_1;
+    else
+        toput += data[ (num - 1) ].Egg_Group_1 + " / " + data[ (num - 1) ].Egg_Group_2;
+    toput += "</type></td></tr></table>"
+    det.innerHTML = toput;
+
+    var hp = document.getElementById( "hp" );
+    // hp.setAttribute( "width", String(data[ (num - 1) ].HP) + "px" );
+    hp.setAttribute( "style", "width: " + String((parseInt(data[ (num - 1) ].HP) * 3)) + "px" );
+
+    var hpnum = document.getElementById( "hp-num" );
+    hpnum.innerHTML = data[ (num - 1) ].HP;
+    // for( var i = 0 ; i < data.length ; i++ )
+    // {
+    //     if( parseInt(data[ i ].Number) == parseInt(el.dataset.number) )
+    //         console.log( data[ i ] );
+    // }
+}
 
 });
 
